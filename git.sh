3a2f7f5fd541c9fead3cd09676b83b70a0102282
networkoperator-gittest <test@cisco.com>
2023-01-24 05:27:11 +0000

    removed ocp410gf

---
+++ git.sh
@@ -0,0 +1,8 @@
+git pull
+rm -rf workload/argo/ocp410gf
+rm -rf workload/config/ocp410gf
+rm -rf workload/servicemesh/ocp410gf
+rm -rf workload/status/ocp410gf
+git add .
+git commit -m "removed ocp410gf"
+git push
