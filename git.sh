657ef0a22861b87c3e08e7e3c2521a4cf90bf168
networkoperator-gittest <test@cisco.com>
2023-02-06 06:32:39 +0000

    removed ocp410bf

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 git pull
-rm -rf workload/argo/alok
-rm -rf workload/config/alok
-rm -rf workload/servicemesh/alok
-rm -rf workload/status/alok
+rm -rf workload/argo/ocp410bf
+rm -rf workload/config/ocp410bf
+rm -rf workload/servicemesh/ocp410bf
+rm -rf workload/status/ocp410bf
 git add .
-git commit -m "removed alok"
+git commit -m "removed ocp410bf"
 git push
