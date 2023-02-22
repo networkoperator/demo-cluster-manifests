b5e90551c620acf1c3341539a614046a5109a1c7
aloverma <ialokverma89@gmail.com>
2023-02-22 02:06:42 -0500

    removed oshift1

--- git.sh
+++ git.sh
@@ -1,17 +1,8 @@
 #git pull
-rm -rf workload/argo/ocp410gf
-rm -rf workload/config/ocp410gf
-rm -rf workload/servicemesh/ocp410gf
-rm -rf workload/status/ocp410gf
-rm -rf workload/argo/alok
-rm -rf workload/config/alok
-rm -rf workload/servicemesh/alok
-rm -rf workload/argo/oshift1
-rm -rf workload/config/oshift1
-rm -rf workload/servicemesh/oshift1
-rm -rf workload/status/oshift1
-
-
+rm -rf workload/argo/ocp410bf
+rm -rf workload/config/ocp410bf
+rm -rf workload/servicemesh/ocp410bf
+rm -rf workload/status/ocp410bf
 git add .
 git commit -m "removed oshift1"
 git push
