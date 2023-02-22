f12ac522bbf8088caf74d62c95f2653f4d536a0d
Rupesh Sahu <rupesh.sahu@oneconvergence.com>
2023-02-22 04:56:39 -0500

    removed oshift1

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #git pull
-rm -rf workload/argo/ocp410bf
-rm -rf workload/config/ocp410bf
-rm -rf workload/servicemesh/ocp410bf
-rm -rf workload/status/ocp410bf
+rm -rf workload/argo/ocp410gf
+rm -rf workload/config/ocp410gf
+rm -rf workload/servicemesh/ocp410gf
+rm -rf workload/status/ocp410gf
 git add .
 git commit -m "removed oshift1"
 git push
