58b45053333f8ae4b7e51bb73cffc540c5171db6
Rupesh Sahu <rupesh.sahu@oneconvergence.com>
2023-02-23 06:10:46 -0500

    removed ocp410gf

--- git.sh
+++ git.sh
@@ -3,10 +3,6 @@ rm -rf workload/argo/ocp410gf
 rm -rf workload/config/ocp410gf
 rm -rf workload/servicemesh/ocp410gf
 rm -rf workload/status/ocp410gf
-rm -rf workload/argo/ocp410bf
-rm -rf workload/config/ocp410bf
-rm -rf workload/servicemesh/ocp410bf
-rm -rf workload/status/ocp410bf
 git add .
 git commit -m "removed ocp410gf"
 git push
