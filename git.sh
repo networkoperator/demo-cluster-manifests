e119fbb0ad4e10973fa297691848c04ac15e01af
root <root@k8s22-ext-rtr.local.lan>
2023-03-02 04:09:49 -0500

    removed alok

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #git pull
-rm -rf workload/argo/ocp410gf
-rm -rf workload/config/ocp410gf
-rm -rf workload/servicemesh/ocp410gf
-rm -rf workload/status/ocp410gf
+rm -rf workload/argo/alok
+rm -rf workload/config/alok
+rm -rf workload/servicemesh/alok
+rm -rf workload/status/alok
 git add .
-git commit -m "removed ocp410gf"
+git commit -m "removed alok"
 git push
