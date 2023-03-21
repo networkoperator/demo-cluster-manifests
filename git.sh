f8496613cb4e97f9bcdd62d2950832cc2c551f7a
aloverma <ialokverma89@gmail.com>
2023-03-21 05:24:42 -0400

    removed alokk8s23

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 #git pull
-rm -rf workload/argo/ocp410gf
-rm -rf workload/config/ocp410gf
-rm -rf workload/servicemesh/ocp410gf
-rm -rf workload/status/ocp410gf
+rm -rf workload/argo/alokk8s23
+rm -rf workload/config/alokk8s23
+rm -rf workload/servicemesh/alokk8s23
+rm -rf workload/status/alokk8s23
 git add .
-git commit -m "removed ocp410gf"
+git commit -m "removed alokk8s23"
 git push
